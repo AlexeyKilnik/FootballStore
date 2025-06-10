da2f684ed182f82f98fecc8d37c3644698a665d6
psigfry <darn.vr@gmail.com>
2025-06-10 18:58:59 +0300

    Лекция 7

---
+++ js/main.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 5; i++){
+    console.log('Цикл for :I равно:',i);
+}
+
+let i = 0;
+while(i < 5){
+    console.log('Цикл While :I равно:',i);
+    i++;
+}
+
+// let arr = []
+
+// arr.forEach()
---
+++ js/products.js
@@ -0,0 +1,16 @@
+const products = [
+    { name: "Футбольный мяч", image:"photo/ball.png", description: "Классический мяч очень удобен!", type: "мяч" },
+    { name: "Футбольные бутсы", image:"photo/boots.png", description: "Классические бутсы очень удобны!", type: "бутсы" },
+    { name: "Вратарские перчатки", image:"photo/gloves.png", description: "Очень удобные перчатки!", type: "перчатки" },
+    { name: "Липкий спрей для перчаток", image:"photo/download.jpg", description: "Спрей для перчаток!", type: "спрей" },
+    { name: "Фишки для тренеровок", image:"photo/fishki.jpg", description: "Фишки для тренировок", type: "фишки" },
+    { name: "Футбольный мяч", image:"photo/ball.png", description: "Ещё один мяч", type: "мяч" },
+    { name: "Футбольная форма", image:"photo/full_31500.jpg", description: "Форма очень удобна!", type: "форма" },
+    { name: "Футбольные бутсы", image:"photo/boots.png", description: "Запасная пара бутс", type: "бутсы" },
+];
+
+const recommendeds = [
+    { name: "Футбольный мяч", image:"photo/ball.png", description: "Классический мяч очень удобен!", type: "мяч" },
+    { name: "Футбольные бутсы", image:"photo/boots.png", description: "Классические бутсы очень удобны!", type: "бутсы" },
+    { name: "Вратарские перчатки", image:"photo/gloves.png", description: "Очень удобные перчатки!", type: "перчатки" },
+];
